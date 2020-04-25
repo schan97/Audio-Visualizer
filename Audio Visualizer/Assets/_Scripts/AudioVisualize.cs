c7ead14b07880961b6db39a0a8a2a34d65b0e6d2
Steven Chan <schan8524@gmail.com>
2020-04-25 03:40:04 -0400

    Add and Implement Open File Button
    Allows users to select an external mp3 file to play

--- Audio Visualizer/Assets/_Scripts/AudioVisualize.cs
+++ Audio Visualizer/Assets/_Scripts/AudioVisualize.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
 
 [RequireComponent (typeof (AudioSource))]
 
@@ -8,6 +10,8 @@ public class AudioVisualize : MonoBehaviour
 {
 	AudioSource _audioSource;
 	public float[] _samples = new float[512];
+
+	string path;
 
     // Start is called before the first frame update
     void Start()
@@ -24,5 +28,27 @@ public class AudioVisualize : MonoBehaviour
 	void GetSpectrumAudioSource()
 	{
 		_audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+	}
+
+	public void OpenSingleFile()
+	{
+
+		path = EditorUtility.OpenFilePanel("Open Mp3 File", "", "mp3");
+		LoadSong();
+
+	}
+
+	public void LoadSong()
+	{
+		if (path != null)
+		{
+			UpdateSong();
+		}
+	}
+
+	public void UpdateSong()
+	{
+		WWW www = new WWW("file:///" + path);
+		_audioSource.clip = NAudioPlayer.FromMp3Data(www.bytes);
 	}
 }
