15df7e86996bcbd149237972b75c337376975621
Steven Chan <schan8524@gmail.com>
2020-04-25 16:11:14 -0400

    Update obsolete function calls in ExtendedFlycam

--- Audio Visualizer/Assets/_Scripts/ExtendedFlycam.cs
+++ Audio Visualizer/Assets/_Scripts/ExtendedFlycam.cs
@@ -32,7 +32,7 @@ public class ExtendedFlycam : MonoBehaviour
 
 	void Start()
 	{
-		Screen.lockCursor = true;
+		Cursor.lockState = CursorLockMode.Locked;
 	}
 
 	void Update()
@@ -66,7 +66,16 @@ public class ExtendedFlycam : MonoBehaviour
 
 		if (Input.GetKeyDown(KeyCode.C))
 		{
-			Screen.lockCursor = (Screen.lockCursor == false) ? true : false;
+
+			if(Cursor.lockState == CursorLockMode.Locked)
+			{
+				Cursor.lockState = CursorLockMode.None;
+			}
+
+			else if (Cursor.lockState == CursorLockMode.None)
+			{
+				Cursor.lockState = CursorLockMode.Locked;
+			}
 		}
 	}
 }
