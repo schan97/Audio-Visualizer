9c69ae53ccb41da39b8c4af3f38020e4592bf692
Steven Chan <schan8524@gmail.com>
2020-04-26 02:02:47 -0400

    Create UIControl script
    Allows users to toggle the UI by pressing T

---
+++ Audio Visualizer/Assets/_Scripts/UIControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour
+{
+	public CanvasGroup canvasGroup;
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.T))
+		{
+			if (canvasGroup.alpha == 1)
+			{
+				canvasGroup.interactable = false;
+				canvasGroup.blocksRaycasts = false;
+				canvasGroup.alpha = 0; 
+			}
+
+			else if (canvasGroup.alpha == 0)
+			{
+				canvasGroup.interactable = true;
+				canvasGroup.blocksRaycasts = true;
+				canvasGroup.alpha = 1;
+			}
+		}
+	}
+}
