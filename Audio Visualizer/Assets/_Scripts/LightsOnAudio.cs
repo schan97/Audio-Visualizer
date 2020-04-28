51ba2aba74a9f5f3c9e812225ec72b612f276b76
Steven Chan <schan8524@gmail.com>
2020-04-28 03:34:14 -0400

    Add LightsOnAudio

---
+++ Audio Visualizer/Assets/_Scripts/LightsOnAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class LightsOnAudio : MonoBehaviour
+{
+	public int band;
+	public float minIntensity, maxIntensity;
+	Light light;
+    // Start is called before the first frame update
+    void Start()
+    {
+		light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(AudioVisualize.audioBand[band] > 0)
+		{
+			light.intensity = (AudioVisualize.audioBandBuffer[band] * (maxIntensity - minIntensity)) + minIntensity;
+		}
+    }
+}
--- Audio Visualizer/Assets/_Scripts/ParamCube.cs
+++ Audio Visualizer/Assets/_Scripts/ParamCube.cs
@@ -7,11 +7,16 @@ public class ParamCube : MonoBehaviour
 	public int band;
 	public float startScale, scaleMultiplier;
 	public bool useBuffer;
+
+	public bool useColorChange;
 	Material material;
 
     void Start()
     {
-
+		if(useColorChange == true)
+		{
+			material = GetComponent<MeshRenderer>().materials[0];
+		}
     }
 
 
@@ -20,6 +25,11 @@ public class ParamCube : MonoBehaviour
 		if (useBuffer == true && AudioVisualize.audioBand[band] > 0)
 		{
 			transform.localScale = new Vector3(transform.localScale.x, (AudioVisualize.audioBandBuffer[band] * scaleMultiplier) + startScale, transform.localScale.z);
+			if (useColorChange == true)
+			{
+				Color color = new Color(AudioVisualize.audioBandBuffer[band], AudioVisualize.audioBandBuffer[band], AudioVisualize.audioBandBuffer[band]);
+				material.SetColor("_EmissionColor", color);
+			}
 		}
 
 		if (useBuffer == false && AudioVisualize.audioBand[band] > 0)
